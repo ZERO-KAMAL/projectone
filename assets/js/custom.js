ca391318e6d11f8f41322a0ee7213ff4e67d159f
kamal khatri <kamal1997khatri@gmail.com>
2022-06-17 21:52:56 +0545

    i think final push

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,20 @@ $(window).scroll(function () {
     $("#header").removeClass("bg-black");
   }
 });
+
+//tabs
+let tabs = document.querySelectorAll(".tab__toggle"),
+  contents = document.querySelectorAll(".tabs__content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    contents.forEach((content) => {
+      content.classList.remove("is-active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("is-active");
+    });
+    contents[index].classList.add("is-active");
+    tabs[index].classList.add("is-active");
+  });
+});
